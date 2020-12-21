43d25253d6b81a6aac57ebdd17858e2d2a1982da
Sadi Abdullah <sadi2abd@gmail.com>
2020-12-21 11:31:54 +1030

    Initial commit

---
+++ MVCApp/Models/EmployeeModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCApp.Models
+{
+    public class EmployeeModel
+    {
+        [Display(Name = "Employee ID")]
+        [Range(100000, 999999, ErrorMessage = "You need to enter a valid employee id")]
+        public int EmployeeId { get; set; }
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "You need to provide this information")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "You need to provide this information")]
+        public string LastName { get; set; }
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email Address")]
+        [Required(ErrorMessage = "You need to provide this information")]
+        public string EmailAddress { get; set; }
+        [Display(Name = "Confirm Email")]
+        [Compare("EmailAddress", ErrorMessage = "The email addess does not match")]
+        public string ConfirmEmail { get; set; }
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "You must have a password")]
+        [DataType(DataType.Password)]
+        [StringLength(20, MinimumLength = 10, ErrorMessage = "Please provide a longer password")]
+        public string Password { get; set; }
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The passwords do not match!")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
